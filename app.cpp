f14ee9c92b8584622dcc7a975aa5e0507d63c59a
MridulSheikh <mridul.sheikh.90@gmail.com>
2022-07-25 02:23:56 +0600

    secure mcash

--- app.cpp
+++ app.cpp
@@ -12,14 +12,14 @@ class BankAccount
 	private:
 		string password;
 	public:
-	       	BankAccount(string account_holder, string address, int age,string password){ //recive user information from client and set data
+	       	BankAccount(string account_holder, string address, int age,string password){
 		this->account_holder=account_holder;
 		this->address=address;
 		this->age=age;
 		this->password=password;
 		this->account_number=rand()%1000000000;
 		this->balance=0;
-		cout<<"Your account no is "<<this->account_number<<endl;	
+		cout<<"Your account no is "<<this->account_number<<endl;
 		}
         	int show_balance(string password)
 		{
@@ -37,6 +37,35 @@ class BankAccount
 			}
 			else
 				cout<<"wrong password please try again"<<endl;
+		}
+		friend class MyCash;
+};
+class MyCash
+{
+	protected:
+		int balance;
+	public:
+		MyCash(){
+			this->balance=0;
+		}
+		void add_money_from_bank(BankAccount *myAccount,string password,int amount)
+		{
+			if(amount>myAccount->balance)
+			{
+			        cout<<"Not enough balance"<<endl;
+			}
+			else{
+				if(myAccount->password == password)
+				{
+				 	this->balance += amount;
+				 	myAccount->balance -= amount;
+					cout<<"Add money from bank is successfull"<<endl;
+				 	cout<<"MyCash blance is "<<this->balance<<" BDT only"<<endl;
+				}
+			        else
+					cout<<"wrong password"<<endl;
+
+			}
 		}
 };
 BankAccount* create_account()
@@ -57,9 +86,20 @@ void add_money(BankAccount *myAccount)
 	myAccount->add_money(password,amount);
 	cout<<"your balance is "<<myAccount->show_balance("abc")<<endl;
 }
+void add_money_from_bank(MyCash *myCash, BankAccount *myAccount)
+{
+	string password;
+	int amount;
+	cout<<"add money from bank"<<endl;
+	cin>>password>>amount;
+	myCash->add_money_from_bank(myAccount,password, amount);
+	cout<<"your avalable bank balance is "<<myAccount->show_balance(password)<<" BDT only"<<endl;
+}
 int main()
 {
 	BankAccount *myAccount = create_account();
 	add_money(myAccount);
+	MyCash *myCash=new MyCash();
+	add_money_from_bank(myCash,myAccount);
 	return 0;
 }
